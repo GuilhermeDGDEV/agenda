69ecdf5d86c906249c17801f907c03917e19f762
GuilhermeDGDEV <guilhermediasgoncalvesdev@gmail.com>
2021-11-14 21:47:21 -0300

    Login and screen styles

--- core/views.py
+++ core/views.py
@@ -1,8 +1,40 @@
 from django.shortcuts import redirect, render
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
+@login_required(login_url='/login/')
 def lista_eventos(request):
-    eventos = Evento.objects.all()
-    response = {'eventos': eventos}
-    return render(request, 'agenda.html', response)
+    usuario = request.user
+
+    if usuario.username == 'admin':
+        eventos = Evento.objects.all()
+    else:
+        eventos = Evento.objects.filter(usuario=usuario)
+
+    dados = {'eventos': eventos}
+    return render(request, 'agenda.html', dados)
+
+
+def login_user(request):
+    return render(request, 'login.html')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, 'Usuário ou senha inválidos')
+    return redirect('/')
